06e2209afb8bc6025502a1534d30b6ccded41611
Nikita Sharma <51536421+niksisharma@users.noreply.github.com>
2022-07-15 12:59:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,37 +7,46 @@ function validation()
     const day1 = document.getElementById('day1');
     const day2 = document.getElementById('day2');
     const day3 = document.getElementById('day3');
+    const emailRegexp = new RegExp(/^[a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1}([a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1})*[a-zA-Z0-9]@[a-zA-Z0-9][-\.]{0,1}([a-zA-Z][-\.]{0,1})*[a-zA-Z0-9]\.[a-zA-Z0-9]{1,}([\.\-]{0,1}[a-zA-Z]){0,}[a-zA-Z0-9]{0,}$/i)
     
     if(!name.value)
     {
-        alert('Please enter your name')
+        alert('Please enter your name');
         name.focus();
+        return false;
     }
 
+    if(!emailRegexp.test(email.value))
+    {
+        alert('Please enter valid email');
+        email.focus();
+        return false;
+    }
+      
     if (phone.value.length != 10 )
     {
-        alert('Phone number should be 10 digits long');
+        alert('Please enter valid (10 digits long) phone number');
         phone.focus();
         return false;
     }
 
     if (nationality.value == '0'){
-        alert('Select Country');
+        alert('Please Select Country');
         nationality.focus();
         return false;
     }
 
     if (occupation.value == '0'){
-        alert('Select an option(Student/Professional)');
+        alert('Please Select an option(Student/Professional)');
         occupation.focus();
         return false;
     }
 
     if(!day1.checked && !day2.checked && !day3.checked)
-{
-    alert('Select at least 1 day');
+    {
+    alert('Please Select at least 1 day');
     return false;
-}
+    }
     
     return true;
 }
